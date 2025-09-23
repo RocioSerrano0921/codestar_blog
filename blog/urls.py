78a1c2b1c4bbc989664eddacffcab02e64ecb12a
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-09-23 20:42:48 +0100

    Add comment editing and deletion functionality; update post detail template and JavaScript for modal interactions

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from django.urls import path  # Import path function from django.urls
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),  # Map the root URL to PostList view
     path('<slug:slug>/', views.post_detail, name='post_detail'),  # Map URL with slug to post_detail view
+     path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'), # Map URL for editing comments to comment_edit view
+     path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'), # Map URL for deleting comments to comment_delete view
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404  # Import render and get_object_or_404 functions
+
+from django.shortcuts import render, get_object_or_404, reverse  # Import render and get_object_or_404 functions
 from django.views import generic  # Import generic views from Django
 from django.contrib import messages  # Import messages framework
-from .models import Post  # Import the Post model
+from django.http import HttpResponseRedirect
+from .models import Post, Comment  # Import the Post and Comment models
 from .forms import CommentForm  # Import the CommentForm form
 
 # Create your views here.
@@ -56,3 +58,45 @@ def post_detail(request, slug):
             "comment_form": comment_form,  # Pass the empty comment form to the template
         },  # Pass the post, comments, comment count, and comment form to the template context
     )
+
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
